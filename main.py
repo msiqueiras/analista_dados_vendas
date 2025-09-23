eb6d8da91a514a1b242a0c956985498cf929de15
msiqueiras <devef.msiqueira@gmail.com>
2025-09-22 22:19:03 -0300

    Décimo primeiro commit: refatoração do código

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pedido
 import cadastros
 import inicializacao
-import media_produtos_uepa
+import medias
 
 print(20*'-', 'PYTHON BITES', 20*'-')
 
@@ -10,56 +10,56 @@ clientes_empresa = []
 metodos_pagamento = ['pix', 'débito', 'crédito', 'dinheiro']
 sistema = True
 while sistema:
-        inicializacao.limpar_tela()
-        inicializacao.menu_principal()
-        try:
-            qual_op_geral = int(input('Digite um código GERAL de operação:'))
+    inicializacao.limpar_tela()
+    inicializacao.menu_principal()
+    try:
+        qual_op_geral = int(input('Digite um código GERAL de operação:'))
 
-            if qual_op_geral == 1:
-                cadastros.menu_cadastro_funcionario(funcionarios_empresa)
+        if qual_op_geral == 1:
+            cadastros.menu_cadastro_funcionario(funcionarios_empresa)
 
-            if qual_op_geral == 2:
-                cadastros.menu_cadastro_clientes(clientes_empresa)
+        if qual_op_geral == 2:
+            cadastros.menu_cadastro_clientes(clientes_empresa)
 
-            if qual_op_geral == 3:
-                print(40*'-')
-                print('CÁRDAPIO PYTHON BITES:')
-                print(40*'-')
-                pedido.cardapio(show = True)
+        if qual_op_geral == 3:
+            print(40*'-')
+            print('CÁRDAPIO PYTHON BITES:')
+            print(40*'-')
+            pedido.cardapio(show = True)
 
-                lista_pedidos = []
-                pedir = True
-                while pedir:
-                    try:
-                        itens_pedidos = int(input('Digite o código do item:'))
-                        lista_pedidos.append(itens_pedidos)
-                        continuar_pedido = str(input('Deseja continuar pedido? [S/N]')).lower().strip()
-                        if continuar_pedido[0] == 'n':
-                            pedir = False
-                            print(40*'-')
-                            print('PEDIDO FINALIZADO')
-                            total_a_pagar, itens_do_relatorio = pedido.vender(*lista_pedidos)
-                            pedido.relatorio(total_a_pagar, itens_do_relatorio)
-                            pedido.pagar_como(metodos_pagamento, total_a_pagar)
-                            print('OBRIGADO E VOLTE SEMPRE!')
-                            
-                    except ValueError:
-                         print('O código digitado não é um número inteiro.')
+            lista_pedidos = []
+            pedir = True
+            while pedir:
+                try:
+                    itens_pedidos = int(input('Digite o código do item:'))
+                    lista_pedidos.append(itens_pedidos)
+                    continuar_pedido = str(input('Deseja continuar pedido? [S/N]')).lower().strip()
+                    if continuar_pedido[0] == 'n':
+                        pedir = False
+                        print(40*'-')
+                        print('PEDIDO FINALIZADO')
+                        total_a_pagar, itens_do_relatorio = pedido.vender(*lista_pedidos)
+                        pedido.relatorio(total_a_pagar, itens_do_relatorio)
+                        pedido.pagar_como(metodos_pagamento, total_a_pagar)
+                        print('OBRIGADO E VOLTE SEMPRE!')
+                        
+                except ValueError:
+                        print('O código digitado não é um número inteiro.')
 
 
-            if qual_op_geral == 4:
-                ticket_medio, ticket_categoria =  media_produtos_uepa.calcular_medias(pedido.cardapio(show = False))
-                print(f'TICKET MÉDIO: R$ {ticket_medio:.2f}')
-                for k, v in ticket_categoria.items():
-                    print(f'Média de {k}.....R${v:.2f}')                       
-                            
+        if qual_op_geral == 4:
+            ticket_medio, ticket_categoria =  medias.calcular_medias(pedido.cardapio(show = False))
+            print(f'TICKET MÉDIO: R$ {ticket_medio:.2f}')
+            for k, v in ticket_categoria.items():
+                print(f'Média de {k.title()}.....R${v:.2f}')                       
+                        
 
-            continuar_operacoes = str(input('Deseja realizar outra operação? [S/N]')).lower().strip()
-            if continuar_operacoes[0] == 'n':
-                sistema = False
-                print('PYTHON BITES ©')
+        continuar_operacoes = str(input('Deseja realizar outra operação? [S/N]')).lower().strip()
+        if continuar_operacoes[0] == 'n':
+            sistema = False
+            print('PYTHON BITES ©')
 
-        except ValueError:
-            print(20*'-')
-            print('Código não reconhecido.')
-            print(20*'-')
+    except ValueError:
+        print(20*'-')
+        print('Código não reconhecido.')
+        print(20*'-')
